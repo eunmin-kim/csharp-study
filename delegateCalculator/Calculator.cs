3bbd9c2832d6778521c29637e01b434e690a8804
eunmin-kim <53runtime@gmail.com>
2024-04-19 19:42:32 +0900

    Delegate 계산기 예제

---
+++ delegateCalculator/Calculator.cs
@@ -0,0 +1,10 @@
+﻿namespace delegateCalculator
+{
+    public class Calculator
+    {
+        public double Add(double x, double y) => x + y;
+        public double Subtract(double x, double y) => x - y;
+        public double Multiply(double x, double y) => x * y;
+        public double Divide(double x, double y) => x / y;
+    }
+}
---
+++ delegateCalculator/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace delegateCalculator
+{
+
+    /// <summary>
+    /// 델리게이트 정의
+    /// </summary>
+    public delegate double MathOperation(double x, double y);
+    
+    
+    
+    /// <summary>
+    /// 델리게이트를 활용한 계산기 프로그램
+    /// </summary>
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Calculator calculator = new Calculator();
+            MathOperation op;
+
+            op = calculator.Add;
+            Console.WriteLine($"Add: {op(10,5)}");
+
+            op = calculator.Divide;
+            Console.WriteLine($"Divide: {op(10,5)}");
+        }
+    }
+}
