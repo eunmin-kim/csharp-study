5ebe5e7c206285f0d2a2e8431fc3a585a437e5a1
eunmin-kim <53runtime@gmail.com>
2024-04-20 22:12:20 +0900

    C# Task 예제

---
+++ TaskExample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TaskExample
+{
+    internal class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Task<int> logRunningTask = new Task<int>(() => CalculateData());
+            Console.WriteLine("다른 작업 수행 중...");
+            int result = await logRunningTask;
+
+            Console.WriteLine($"결과: {result}");
+
+        }
+
+        private static int CalculateData()
+        {
+            // 복잡한 계산을 가정
+            System.Threading.Thread.Sleep(2000); // 시뮬레이션을 위한 딜레이
+            return 123;
+        }
+    }
+}
