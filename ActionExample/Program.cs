21b2da9b33cb752442b861913db8a617ffbc8050
eunmin-kim <53runtime@gmail.com>
2024-04-21 17:00:40 +0900

    C# Action 델리게이트 예제

---
+++ ActionExample/Program.cs
@@ -0,0 +1,18 @@
+﻿
+namespace ActionExample;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        // 파라미터가 없는 Action
+        Action printHello = () => Console.WriteLine("Hello, Action!");
+        printHello();
+        
+        // 파라미터가 있는 Action
+
+        Action<int, int> printSum = (int a, int b) => Console.WriteLine($"Sum Result: {a + b}");
+        printSum(1, 2);
+    }
+}
